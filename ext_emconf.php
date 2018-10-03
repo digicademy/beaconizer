ded01ebaf22f9a808065139915eb7c0d5af7b18f
metacontext <Torsten.Schrade@adwmainz.de>
2018-10-03 17:42:58 +0200

    Bring ext_emconf into modern format

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,34 +1,20 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Beaconizer',
-    'description' => 'Harvest data from BEACON files and BEACONize your data. See: https://meta.wikimedia.org/wiki/Dynamic_links_to_external_resources',
+    'description' => 'Harvest data from BEACON files and beaconize your data. See: https://meta.wikimedia.org/wiki/Dynamic_links_to_external_resources',
     'category' => 'fe',
     'author' => 'Torsten Schrade',
     'author_email' => 'Torsten.Schrade@adwmainz.de',
-    'shy' => '',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'module' => '',
-    'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
     'author_company' => 'Academy of Sciences and Literature | Mainz',
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
     'version' => '1.0.0',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '7.6.0-8.7.99',
-            'extbase' => '',
-            'fluid' => '',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-    '_md5_values_when_last_written' => 'a:2:{s:12:"ext_icon.gif";s:4:"bbb3";s:40:"Classes/ViewHelpers/FilterViewHelper.php";s:4:"47e2";}',
-    'suggests' => array(),
-);
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
